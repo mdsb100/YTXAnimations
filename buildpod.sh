253fd49298feaaae75ef3e6791e9a26a9b047b5b
lilonglong <596927598@qq.com>
2016-04-11 18:35:31 +0800

    Update buildpod.sh

--- buildpod.sh
+++ buildpod.sh
@@ -2,8 +2,8 @@ pushd "$(dirname "$0")" > /dev/null
 SCRIPT_DIR=$(pwd -L)
 popd > /dev/null
 
-echo "publish repo YTXChart"
-pod repo push baidao-ios-ytx-pod-specs YTXAnimation.podspec --verbose
+echo "publish repo YTXAnimations"
+pod repo push baidao-ios-ytx-pod-specs YTXAnimations.podspec --verbose
 
 ret=$?
 
